86e909b824bdbd2d110000416529741bee208400
Michael Hankin <mjhankin1@gmail.com>
2020-02-07 07:24:17 -0600

    move message handling logic to bot class

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 class Bot:
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, slack_client):
+        self.slack_client = slack_client
 
-    def handle_message(message):
-        # TODO: handle message
+    def handle_message(self, message):
+        if "hi" in message.get("text"):
+            channel = message["channel"]
+            message = "Hello <@%s>!" % message["user"]
+            self.slack_client.chat_postMessage(channel=channel, text=message)
